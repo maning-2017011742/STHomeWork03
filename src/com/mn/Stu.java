4b7e9f05e9e4a6ccd8e0e5e91ae8fcc7ef2dead7
maning-2017011742 <64588331+maning-2017011742@users.noreply.github.com>
2020-05-18 23:15:07 +0800

    Create Stu.java

---
+++ src/com/mn/Stu.java
@@ -0,0 +1,71 @@
+package com.mn;
+
+public class Stu {
+	//��Ա����
+//	���⣺3.5
+//	private int Id;//ѧ��
+	  private int Id;//ѧ��
+	  private String name;//����
+	  private boolean gender;//�Ա�
+	  private String birDate;//����
+//	  public Stu() {
+//		    super();
+//		  }
+//	  ���⣺2.5��3.5
+//	  public int getId() {
+//	    return Id;
+//	  }
+//	  public void setId(int Id) {
+//	    this.Id = Id;
+//	  }
+//	  public String getName() {
+//		    return name;
+//		  }
+//	  public void setName(String name) {
+//	    this.name = name;
+//	  }  
+//	  public boolean getGender() {
+//	    return gender;
+//	  }
+//	  public void setGender(boolean gender) {
+//	    this.gender = gender;
+//	  }
+//	  public String getBirDate() {
+//	    return birDate;
+//	  }
+//	  public void setBirDate(String birDate) {
+//	    this.birDate = birDate;
+//	  }
+//	  ���ܣ���ȡ������ѧ��ѧ��
+//	  ���룺����
+	  public int getId() {
+	    return Id;
+	  }
+	  public void setId(int Id) {
+	    this.Id = Id;
+	  }
+//	  ���ܣ���ȡ������ѧ������
+//	  ���룺�ַ���
+	  public String getName() {
+		    return name;
+		  }
+	  public void setName(String name) {
+	    this.name = name;
+	  }  
+//	  ���ܣ���ȡ������ѧ���Ա�
+//	  ���룺������
+	  public boolean getGender() {
+	    return gender;
+	  }
+	  public void setGender(boolean gender) {
+	    this.gender = gender;
+	  }
+//	  ���ܣ���ȡ������ѧ������
+//	  ���룺�ַ���
+	  public String getBirDate() {
+	    return birDate;
+	  }
+	  public void setBirDate(String birDate) {
+	    this.birDate = birDate;
+	  }
+}
