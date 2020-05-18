6d062ebad57063f60caeb519d380e46f02e5bc11
maning-2017011742 <64588331+maning-2017011742@users.noreply.github.com>
2020-05-18 23:10:33 +0800

    Delete SMTest.java

--- SMTest.java
+++
@@ -1,7 +0,0 @@
-package com.mn;
-	public class SMTest {
-		 public static void main(String[] args) {
-		        StudentManager studentManager =new StudentManager();
-		        studentManager.App();
-		    }
-}
