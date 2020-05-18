be82570c79a2caaceb2be32c208857c5c9d6b3b8
maning-2017011742 <64588331+maning-2017011742@users.noreply.github.com>
2020-05-18 23:14:35 +0800

    Create SMTest.java

---
+++ src/com/mn/SMTest.java
@@ -0,0 +1,15 @@
+package com.mn;
+	public class SMTest {
+//		���⣺2.5
+//		 public static void main(String[] args) {
+//		        StudentManager studentManager =new StudentManager();
+//		        studentManager.App();
+//		    }
+//		������
+//		���ܣ�����StudentManager.java�е�App����������ʼִ�С�
+//		�޷���ֵ
+		public static void main(String[] args){
+	        StudentManager studentManager =new StudentManager();
+	        studentManager.App();
+	    }
+}
