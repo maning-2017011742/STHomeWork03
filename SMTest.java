330f709400853219a1ca2bd0e060ad4a92e8622a
maning-2017011742 <64588331+maning-2017011742@users.noreply.github.com>
2020-05-18 23:02:24 +0800

    Add files via upload

---
+++ SMTest.java
@@ -0,0 +1,7 @@
+package com.mn;
+	public class SMTest {
+		 public static void main(String[] args) {
+		        StudentManager studentManager =new StudentManager();
+		        studentManager.App();
+		    }
+}
